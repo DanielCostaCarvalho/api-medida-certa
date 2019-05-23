44a8897000dd0799076a83d5db9cbefeeb777afc
Daniel Carvalho <danielcostacarvalho@gmail.com>
2019-05-23 17:29:05 -0300

    Listagem e detalhes do tipo da roupa

---
+++ src/controllers/tipoRoupa.js
@@ -0,0 +1,28 @@
+const client = require('../database');
+
+const listagem = async (req, res) => {
+  client.query('SELECT * FROM tipoRoupa', (error, results) => {
+    if (error) {
+      console.log("deu ruim :/");
+      throw error;
+    }
+    return res.status(200).json(results.rows);
+  })
+};
+
+const mostrar = async (req, res) => {
+  const { idTipoRoupa } = req.params;
+  const comando = "SELECT * FROM tipoRoupa where idTipoRoupa = $1";
+  client.query(comando, [idTipoRoupa], (error, results) => {
+    if (error) {
+      console.log("deu ruim :/");
+      throw error;
+    }
+    return res.status(200).json(results.rows);
+  })
+};
+
+module.exports = {
+  listagem,
+  mostrar
+};
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const preco = require('./controllers/preco');
 const loja = require('./controllers/loja');
 const cliente = require('./controllers/cliente');
+const tipoRoupa = require("./controllers/tipoRoupa");
 
 //Preco
 router.get('/preco/listagem', preco.listagem);
@@ -15,5 +16,8 @@ router.get('/cliente/mostrar/:idCliente', cliente.mostrar);
 router.get('/cliente/listagem', cliente.listagem);
 router.post('/cliente/cadastrar', cliente.cadastrar);
 router.put('/cliente/atualizar/:idCliente', cliente.atualizar);
+//Tipo de roupa
+router.get('/tipoRoupa/mostrar/:idTipoRoupa', tipoRoupa.mostrar);
+router.get('/tipoRoupa/listagem', tipoRoupa.listagem);
 
 module.exports = router;
