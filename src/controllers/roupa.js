668409a1e0438affae90dd91d7ce648374eda2d1
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2019-06-07 10:09:20 -0300

    Adicionar nome da roupa

--- src/controllers/roupa.js
+++ src/controllers/roupa.js
@@ -3,7 +3,7 @@ const format = require('pg-format');
 
 const listagemPendentes = async (req, res) => {
   try{
-    await client.query('SELECT * FROM roupa where idroupa in (select distinct(idroupa) from ajuste where datafinalizacao is null)').then(resp => {
+    await client.query('SELECT r.*, t.nomeroupa FROM roupa r inner join tipoRoupa t on r.idtiporoupa = t.idtiporoupa where concluido <> true').then(resp => {
       const completo = [];
       client.query('select * from ajuste where datafinalizacao is null').then(resposta => {
         //dentro do for colocar um filter para ver se o idroupa do resposta = ao do r, se for adicionar em um array e por fim colocar o array no objeto e dar push
@@ -25,7 +25,7 @@ const listagemPendentes = async (req, res) => {
 
 const listagemPedido = async (req, res) => {
   const { idPedido } = req.params;
-  const comando = "SELECT * FROM roupa where idpedido = $1";
+  const comando = "SELECT r.*, t.nomeroupa FROM roupa r inner join tipoRoupa t on r.idtiporoupa = t.idtiporoupa where idpedido = $1";
   client.query(comando, [idPedido], (error, results) => {
     if (error) {
       console.log(error);
@@ -36,7 +36,7 @@ const listagemPedido = async (req, res) => {
 };
 
 const listagemNaoEntregues = async (req, res) => {
-  client.query('SELECT r.* FROM ajuste a inner join roupa r on a.idroupa =  r.idroupa where r.dataentrega is null and a.datafinalizacao is not null', (error, results) => {
+  client.query('SELECT r.*, t.nomeroupa FROM ajuste a inner join roupa r on a.idroupa =  r.idroupa inner join tipoRoupa t on r.idtiporoupa = t.idtiporoupa where r.dataentrega is null and a.datafinalizacao is not null', (error, results) => {
     if (error) {
       console.log(error);
       return res.status(404).send();
@@ -47,7 +47,7 @@ const listagemNaoEntregues = async (req, res) => {
 
 const mostrar = async (req, res) => {
   const { idRoupa } = req.params;
-  const comando = "SELECT * FROM roupa where idroupa = $1";
+  const comando = "SELECT r.*, t.nomeroupa FROM roupa r inner join tipoRoupa t on r.idtiporoupa = t.idtiporoupa  where idroupa = $1";
   client.query(comando, [idRoupa], (error, results) => {
     if (error) {
       console.log(error);
